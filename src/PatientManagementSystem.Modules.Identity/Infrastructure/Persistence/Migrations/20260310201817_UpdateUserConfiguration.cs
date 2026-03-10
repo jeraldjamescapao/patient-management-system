d530f38d7785aa6bbb005976ed2271e75de552dd
Jerald James Capao <2690162+jeraldjamescapao@users.noreply.github.com>
2026-03-10 21:38:55 +0100

    chore(identity): generate EF Core migration for updated User configuration - add migration: UpdateUserConfiguration - update IdentityDbContextModelSnapshot

---
+++ src/PatientManagementSystem.Modules.Identity/Infrastructure/Persistence/Migrations/20260310201817_UpdateUserConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientManagementSystem.Modules.Identity.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "users",
+                newName: "id");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_active",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "users",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_active",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: true);
+        }
+    }
+}
